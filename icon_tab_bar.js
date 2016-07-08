b9ebd0f9c44da21beb9008bf41b3f847e5852aeb
Evan Petousis <evan@petousis.net>
2016-07-08 16:01:49 +1000

    Implemented icon tab bar

---
+++ icon_tab_bar.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+/**
+ * Modified from the FacebookExample class FacebookTabBar,
+ * from the react-native-scrollable-tab-view repository.
+ * https://github.com/skv-headless/react-native-scrollable-tab-view/blob/master/examples/FacebookTabsExample/FacebookTabBar.js
+ * Copyright 2016 skv-headless.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+const IconTabBar = React.createClass({
+  tabIcons: [],
+
+  propTypes: {
+    goToPage: React.PropTypes.func,
+    activeTab: React.PropTypes.number,
+    tabs: React.PropTypes.array,
+  },
+
+  componentDidMount() {
+    if (this.props.activeTextColor.substring(0,1) !== "#" || this.props.inactiveTextColor.substring(0,1) !== "#") {
+      global.ErrorUtils.reportFatalError({name: "IncompatibleColorTypeError", message: "Non-hex active/inactive text colors are not currently supported in IconTabBar."});
+    }
+    this._listener = this.props.scrollValue.addListener(this.setAnimationValue);
+  },
+
+  setAnimationValue({ value, }) {
+    this.tabIcons.forEach((icon, i) => {
+      const altProgress = (Math.abs(value - i) >= 0 && Math.abs(value - i) <= 1) ? Math.abs(value-i) : 1;
+      const progress = (value - i >= 0 && value - i <= 1) ? value - i : altProgress;
+      icon.setNativeProps({
+        style: {
+          color: this.iconColor(progress),
+        },
+      });
+    });
+  },
+
+  hexToRgb(hex) {
+    // Code taken from StackOverflow answer: http://stackoverflow.com/a/5624139
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+  },
+
+  //color between rgb(59,89,152) and rgb(204,204,204)
+  iconColor(progress) {
+    const active = this.hexToRgb(this.props.activeTextColor);
+    const inactive = this.hexToRgb(this.props.inactiveTextColor);
+    const red = active["r"] + (inactive["r"] - active["r"]) * progress;
+    const green = active["g"] + (inactive["g"] - active["g"]) * progress;
+    const blue = active["b"] + (inactive["b"] - active["b"]) * progress;
+    return `rgb(${red}, ${green}, ${blue})`;
+  },
+
+  render() {
+    console.log(this.props);
+    return <View style={[styles.tabs, this.props.style, {backgroundColor: this.props.backgroundColor}]}>
+      {this.props.tabs.map((tab, i) => {
+        return <TouchableOpacity key={tab} onPress={() => this.props.goToPage(i)} style={styles.tab}>
+          <Icon
+            name={tab}
+            size={30}
+            color={this.props.activeTab === i ? this.props.activeTextColor : this.props.inactiveTextColor}
+            ref={(icon) => { this.tabIcons[i] = icon; }}
+          />
+        </TouchableOpacity>;
+      })}
+    </View>;
+  },
+});
+
+const styles = StyleSheet.create({
+  tab: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingBottom: 10,
+  },
+  tabs: {
+    height: 45,
+    flexDirection: 'row',
+    paddingTop: 5,
+    borderWidth: 1,
+    borderTopWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderBottomColor: 'rgba(0,0,0,0.05)',
+  },
+});
+
+export default IconTabBar;
